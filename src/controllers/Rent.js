8af5f16f542d402c38d8bfe0cc92404e72bc9700
OinsaneX <alejandroalfonsovargas@gmail.com>
2021-07-04 15:11:42 -0700

    Updt

--- src/controllers/Rent.js
+++ src/controllers/Rent.js
@@ -1,4 +1,5 @@
 const rentModel = require('../models/Rent')
+const carModel = require('../models/Car')
 const rentCtrlr = {}
 
 rentCtrlr.getRents = async (req,res) =>{
@@ -7,13 +8,13 @@ rentCtrlr.getRents = async (req,res) =>{
 }
 
 rentCtrlr.createRent = async (req,res) =>{
-    const {idCard,idUser,pickUp,dropOff,price} = req.body
+    const {idCar,idUser,pickUp,dropOff,price} = req.body
 
     const newRent = new rentModel({
-        idCard,idUser,pickUp,dropOff,price
+        idCar,idUser,pickUp,dropOff,price
     })
 
-    await rentModel.save()
+    await newRent.save()
     .then((response) => res.json({message:response}))
     .catch((err) =>res.json({message:err}))
 }
@@ -22,6 +23,30 @@ rentCtrlr.deleteRent = async (req,res) =>{
     const id = req.params.id
 
     await rentModel.findByIdAndDelete(id)
+
+}
+rentCtrlr.searchCarsavailable = async (req,res) =>{
+    const {pickUp,dropOff} = req.body
+    const cars =await carModel.find()
+    const rents =await rentModel.find()
+    const listAvailable = []
+    cars.forEach(car => {
+        var available = true
+        rents.forEach(rent => {
+            if(rent.idCar==car._id){
+                if(new Date(rent.pickUp)>=new Date(pickUp) && new Date(rent.dropOff)<=new Date(dropOff)){
+                    available = false
+                }     
+            }
+
+           
+        })
+        if(available){
+        listAvailable.push(car)
+        }
+    })
+    res.json(listAvailable)
+    
 
 }
 
--- src/routes/rent.js
+++ src/routes/rent.js
@@ -1,11 +1,14 @@
 const {Router} = require('express')
 const router = Router()
-const {getRents,deleteRent,createRent} = require('../controllers/Rent')
+const {getRents,deleteRent,createRent,searchCarsavailable} = require('../controllers/Rent')
 
 router.route("/")
 .get(getRents)
 .post(createRent)
 router.route("/:id")
 .delete(deleteRent)
+
+router.route("/searchAvailable")
+.post(searchCarsavailable)
 
 module.exports = router
