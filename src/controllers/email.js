6c4a5aa6aa6d54d06ca5b3fb006cad93faf9eda3
OinsaneX <alejandroalfonsovargas@gmail.com>
2021-07-07 13:23:13 -0700

    main Updt

--- src/controllers/email.js
+++ src/controllers/email.js
@@ -5,7 +5,6 @@ emailCtrl.sendTestEmail = async (req,res) =>{
 nodemailer.createTestAccount((err, account) =>{
     const htmlEmail = `
     <h2>Email enviado automaticamente desde Rent_A_Car</h2>
-    <img src="https://th.bing.com/th/id/R.fac273276de2be63dece6b444aeefb86?rik=fLhDXMNwvYNiaQ&riu=http%3a%2f%2f3.bp.blogspot.com%2f--d8w6Xaxe9M%2fVeO-YpZoezI%2fAAAAAAAADXI%2fjqZRfnoIWH8%2fs1600%2frent-a-car-compressed.jpg&ehk=aZSX6iRRM7QEgwQr1DiKWIzLyjSMnJbnEoC2xvP7%2bNg%3d&risl=&pid=ImgRaw" width="200" alt="" />
     <h3>Gracias por crearte una cuenta en nuestro sitio ... Active las notificaciones en su cuenta para recibir notificaciones de ofertas</h3>
     `;
     let transporter = nodemailer.createTransport({
