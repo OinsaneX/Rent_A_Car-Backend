75b9b5d1d53d6724102fc4fa6530f1ba88ec2a70
AlejandroVargas_Dev <alejandroalfonsovargas@gmail.com>
2021-10-21 21:23:02 -0300

    asd

--- src/controllers/User.js
+++ src/controllers/User.js
@@ -1,81 +1,142 @@
-const userModel = require('../models/User')
-const rentModel = require('../models/Rent')
-const userCtrl = {}
+const userModel = require("../models/User");
+const rentModel = require("../models/Rent");
+const userCtrl = {};
 
-userCtrl.getUsers = async(req,res)=>{
-    const userList = await userModel.find({deleted:false})
-    res.json(userList)
-}
+userCtrl.getUsers = async (req, res) => {
+  const userList = await userModel.find({ deleted: false });
+  res.json(userList);
+};
 
-userCtrl.createUser = async(req,res)=>{
-    const {name,username,email,identity,phone,password,nacionality,country,address,role,pasport} = req.body
-    const userExist = await userModel.find({username})
-    const identityExist = await userModel.find({identity})
-   
-     if(userExist.length > 0){
-        res.json({errUser:true})
-    }
-    else if(identityExist.length > 0){
-        res.json({errIdentity:true})
-    }
-    else{
-    const newUser = new userModel({name,username,email,identity,phone,password,nacionality,country,address,role,pasport})
-   await newUser.save()
-   .then(response=>res.json(response))
-   .catch(err=>res.json({message:err}))
-    }
-}
+userCtrl.createUser = async (req, res) => {
+  const {
+    name,
+    username,
+    email,
+    identity,
+    phone,
+    password,
+    nacionality,
+    country,
+    address,
+    role,
+    pasport,
+  } = req.body;
+  const userExist = await userModel.find({ username });
+  const identityExist = await userModel.find({ identity });
 
-userCtrl.deleteUser = async(req,res)=>{
-    const id = req.params.id
-    await userModel.findByIdAndUpdate(id,{deleted:true})
-    await rentModel.updateMany({idUser:id},{active:false})
-    res.json("delete")
-}
+  if (userExist.length > 0) {
+    res.json({ errUser: true });
+  } else if (identityExist.length > 0) {
+    res.json({ errIdentity: true });
+  } else {
+    const newUser = new userModel({
+      name,
+      username,
+      email,
+      identity,
+      phone,
+      password,
+      nacionality,
+      country,
+      address,
+      role,
+      pasport,
+    });
+    await newUser
+      .save()
+      .then((response) => res.json(response))
+      .catch((err) => res.json({ message: err }));
+  }
+};
+userCtrl.updateUser = async (req, res) => {
+  const { id } = req.params;
+  const {
+    name,
+    username,
+    email,
+    identity,
+    phone,
+    password,
+    nacionality,
+    country,
+    address,
+    role,
+    pasport,
+  } = req.body;
+  const userExist = await userModel.find({ username });
+  const identityExist = await userModel.find({ identity });
 
-userCtrl.getUserById = async(req,res)=>{
-    const id = req.params.id
+  if (userExist.length > 0) {
+    res.json({ errUser: true });
+  } else if (identityExist.length > 0) {
+    res.json({ errIdentity: true });
+  } else {
+    const newUser = new userModel({
+      name,
+      username,
+      email,
+      identity,
+      phone,
+      password,
+      nacionality,
+      country,
+      address,
+      role,
+      pasport,
+    });
+    await userModel
+      .findByIdAndUpdate(id, { ...newUser })
+      .then((response) => res.json(response))
+      .catch((err) => res.json({ message: err }));
+  }
+};
 
-    const user = await userModel.findById(id)
-    res.json(user)
-}
+userCtrl.deleteUser = async (req, res) => {
+  const id = req.params.id;
+  await userModel.findByIdAndUpdate(id, { deleted: true });
+  await rentModel.updateMany({ idUser: id }, { active: false });
+  res.json("delete");
+};
 
+userCtrl.getUserById = async (req, res) => {
+  const id = req.params.id;
 
+  const user = await userModel.findById(id);
+  res.json(user);
+};
 
-userCtrl.getUserByNameAndPass = async(req,res)=>{
-    const {username ,password} = req.body
+userCtrl.getUserByNameAndPass = async (req, res) => {
+  const { username, password } = req.body;
 
-  const userSelected = await userModel.findOne({username,password})
-    res.json(userSelected)
-}
+  const userSelected = await userModel.findOne({ username, password });
+  res.json(userSelected);
+};
 
-userCtrl.convertToDriver = async(req,res)=>{
-    const {id} = req.params
+userCtrl.convertToDriver = async (req, res) => {
+  const { id } = req.params;
 
-    const {experience_years,license,licenseValidation} = req.body
-try {
-    await userModel.findByIdAndUpdate(id,{role:'driver',experience_years,license,licenseValidation})
+  const { experience_years, license, licenseValidation } = req.body;
+  try {
+    await userModel.findByIdAndUpdate(id, {
+      role: "driver",
+      experience_years,
+      license,
+      licenseValidation,
+    });
+  } catch (error) {
+    res.send("err");
+  }
+};
 
-} catch (error) {
-    res.send("err")
-}  
+userCtrl.confirmAccount = async (req, res) => {
+  const { id } = req.params;
 
-}
+  try {
+    await userModel.findByIdAndUpdate(id, { confirmed: true });
+    res.send("updated");
+  } catch (err) {
+    res.send("error");
+  }
+};
 
-
-
-userCtrl.confirmAccount = async(req,res)=>{
-    const {id} = req.params
-    
-try{
-    await userModel.findByIdAndUpdate(id,{confirmed:true})
-    res.send("updated")
-}catch(err){
-    res.send("error")
-} 
-   
-}
-
-
-
-module.exports = userCtrl
+module.exports = userCtrl;
--- src/routes/user.js
+++ src/routes/user.js
@@ -1,22 +1,24 @@
-const {Router} = require('express')
-const router = Router()
-const {getUsers,getUserById,getUserByNameAndPass,deleteUser,createUser,convertToDriver,confirmAccount} = require('../controllers/User')
+const { Router } = require("express");
+const router = Router();
+const {
+  getUsers,
+  getUserById,
+  getUserByNameAndPass,
+  updateUser,
+  deleteUser,
+  createUser,
+  convertToDriver,
+  confirmAccount,
+} = require("../controllers/User");
 
-router.route("/")
-.get(getUsers)
-.post(createUser)
+router.route("/").get(getUsers).post(createUser);
 
-router.route("/:id")
-.get(getUserById)
-.delete(deleteUser)
+router.route("/:id").get(getUserById).put(updateUser).delete(deleteUser);
 
-router.route("/login")
-.post(getUserByNameAndPass)
+router.route("/login").post(getUserByNameAndPass);
 
-router.route("/convertToDriver/:id")
-.put(convertToDriver)
+router.route("/convertToDriver/:id").put(convertToDriver);
 
-router.route("/confirmAccount/:id")
-.get(confirmAccount)
+router.route("/confirmAccount/:id").get(confirmAccount);
 
-module.exports = router
+module.exports = router;
