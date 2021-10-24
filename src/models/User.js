ff6a0b25497bc98f23a6a5e6415ee447bfb2ab90
AlejandroVargas_Dev <alejandroalfonsovargas@gmail.com>
2021-10-24 17:29:33 -0300

    asd

--- src/models/User.js
+++ src/models/User.js
@@ -1,66 +1,65 @@
-const {Schema , model} = require('mongoose')
+const { Schema, model } = require("mongoose");
 
 const userSchema = new Schema({
-  
-    name:{
-        type:String,
-        required:true
-    },
-    username:{
-        type:String,
-        required:true
-    },
-    email:{
-        type:String,
-        required:true
-    },
-    identity:{
-        type:String,
-        required:true
-    },
-    phone:{
-        type:String,
-        required:true
-    },
-    password:{
-        type:String,
-        required:true
-    },
-    nacionality:{
-        type:String,
-        required:true
-    },
-    country:{
-        type:String,
-        required:true
-    },
-    address:{
-        type:String,
-        required:true
-    },
-    role:{
-        type:String,
-        default:"client"
-    },
-      deleted:{
-        type:Boolean,
-        default:false
-      },
-      experience_years:{
-        type:Number,
-      },
-      license:{
-        type:String,
-      },
-      licenseValidation:{
-        type:String,
-      },
-      confirmed:{
-          type:Boolean,
-          default:false
-      }
-    })
+  name: {
+    type: String,
+    required: true,
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  identity: {
+    type: String,
+  },
+  phone: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  pasport: {
+    type: String,
+  },
+  nacionality: {
+    type: String,
+    required: true,
+  },
+  country: {
+    type: String,
+    required: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    default: "client",
+  },
+  deleted: {
+    type: Boolean,
+    default: false,
+  },
+  experience_years: {
+    type: Number,
+  },
+  license: {
+    type: String,
+  },
+  licenseValidation: {
+    type: String,
+  },
+  confirmed: {
+    type: Boolean,
+    default: false,
+  },
+});
 
-
-
-module.exports = model("user",userSchema)
+module.exports = model("user", userSchema);
