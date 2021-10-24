4de1de8f884b53a171b5ae90d2cc9a16a6b91552
AlejandroVargas_Dev <alejandroalfonsovargas@gmail.com>
2021-10-24 17:16:59 -0300

    asd

--- src/controllers/Car.js
+++ src/controllers/Car.js
@@ -1,66 +1,108 @@
-const carModel = require("../models/Car")
-const carController = {}
+const carModel = require("../models/Car");
+const carController = {};
 
-carController.getCars = async(req, res) =>{
- 
-const carList = await carModel.find()
-console.log(carList)
-res.json(carList)
-}
+carController.getCars = async (req, res) => {
+  const carList = await carModel.find();
+  console.log(carList);
+  res.json(carList);
+};
 
-carController.addCar = async(req, res) =>{
+carController.addCar = async (req, res) => {
+  const {
+    brand,
+    model,
+    description,
+    price_per_day,
+    registration,
+    imageUrl,
+    qualify,
+    ports,
+    air,
+    type,
+    transmission,
+    capacity,
+  } = req.body;
 
-    const {brand,model, description,price_per_day,registration,imageUrl,qualify,ports,air,type,transmission,capacity} = req.body
-
+  const registrationExist = await carModel.find({ registration });
+  console.log(registrationExist.length);
+  if (registrationExist.length > 0) {
+    res.json({ registrationEx: true });
+  } else {
     const newCar = new carModel({
-        brand,
-        model,
-        description,
-        registration,
-        price_per_day,
-        imageUrl,
-        qualify,
-        ports,
-        air,
-        type,
-        transmission,
-        capacity
-    })
-    console.log(newCar)
-    await newCar.save()
-    .then(()=>{
-        res.json({isOk: true})
-    })
-    .catch((err)=>{
-        res.json({isOk:false})
-    })
-}
+      brand,
+      model,
+      description,
+      registration,
+      price_per_day,
+      imageUrl,
+      qualify,
+      ports,
+      air,
+      type,
+      transmission,
+      capacity,
+    });
+    console.log(newCar);
+    await newCar
+      .save()
+      .then(() => {
+        res.json({ isOk: true });
+      })
+      .catch((err) => {
+        res.json({ isOk: false });
+      });
+  }
+};
 
-carController.getCar = async(req, res) =>{
-    const id = req.params.id
+carController.getCar = async (req, res) => {
+  const id = req.params.id;
 
-    const car =await carModel.findById(id)
-    
-    res.json(car)
-}
+  const car = await carModel.findById(id);
 
-carController.deleteCar = async(req, res) =>{
-    const id = req.params.id
+  res.json(car);
+};
 
-    await carModel.findByIdAndDelete(id)
-    
-    res.json({isOk:true})
+carController.deleteCar = async (req, res) => {
+  const id = req.params.id;
 
-}
+  await carModel.findByIdAndDelete(id);
 
-carController.updateCar = async(req, res) =>{
-    const id = req.params.id
-    const {brand, model, description,price_per_day,imageUrl,ports,air,type,transmission,capacity,registration} = req.body
+  res.json({ isOk: true });
+};
 
-    const newCar = new carModel({_id:id,brand, model, description, price_per_day,registration, imageUrl,ports,air,type,transmission,capacity})
-    await carModel.findByIdAndUpdate(id,newCar)
+carController.updateCar = async (req, res) => {
+  const id = req.params.id;
+  const {
+    brand,
+    model,
+    description,
+    price_per_day,
+    imageUrl,
+    ports,
+    air,
+    type,
+    transmission,
+    capacity,
+    registration,
+  } = req.body;
 
-    res.json({isOk:true})
-}
+  const newCar = new carModel({
+    _id: id,
+    brand,
+    model,
+    description,
+    price_per_day,
+    registration,
+    imageUrl,
+    ports,
+    air,
+    type,
+    transmission,
+    capacity,
+  });
+  await carModel.findByIdAndUpdate(id, newCar);
 
-module.exports = carController
+  res.json({ isOk: true });
+};
+
+module.exports = carController;
